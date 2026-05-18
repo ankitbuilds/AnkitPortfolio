2f02694f92efa82f747d5ad25ad663889e51e110
Ankit Mishra <ankitmishra.akmishra@gmail.com>
2026-05-18 16:00:32 +0530

    add projects in work section

--- src/assets/Work_data.js
+++ src/assets/Work_data.js
@@ -1,6 +1,6 @@
 import project1_img from "../assets/chat.png";
 import project2_img from "../assets/social.png";
-import project3_img from "../assets/project_3.svg";
+import project3_img from "../assets/Gen.png";
 // import project4_img from "../assets/project_4.svg";
 // import project5_img from "../assets/project_5.svg";
 // import project6_img from "../assets/project_6.svg";
@@ -9,7 +9,7 @@ import project3_img from "../assets/project_3.svg";
 const Work_data = [
     { w_no: "01", w_name: "Real Time Chat Web App", w_desc: "Built a real-time chat app with secure authentication, instant messaging using MERN stack, Socket.IO, and responsive UI.", w_img: project1_img, w_link: "https://chat-app-dzp2.onrender.com/" },
     { w_no: "02", w_name: "Social Media Web App", w_desc: "Developed a social media web app supporting posts, likes, comments, and follows with scalable backend, responsive design, and optimized performance.", w_img: project2_img, w_link: "https://dev-hub-fawn.vercel.app/" },
-    { w_no: "02", w_name: "Web design", w_img: project3_img },
+    { w_no: "02", w_name: "Gen AI Resume Analyzer", w_desc: "AI-powered resume analyzer that evaluates resumes, matches them with job descriptions, and provides smart improvement suggestions.",w_img: project3_img, w_link:"https://gen-ai-resume-analyzer-g7u0.onrender.com/"},
     // {w_no:"02", w_name:"Web design", w_img:project4_img},
     // {w_no:"02", w_name:"Web design", w_img:project5_img},
     // {w_no:"02", w_name:"Web design", w_img:project6_img},
